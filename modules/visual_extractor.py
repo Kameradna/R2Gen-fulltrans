06d4d1f82db80a8146424a558a0c9c34698de217
Kameradna <pjicasper@gmail.com>
2022-08-31 15:06:33 +1000

    original tag added

--- modules/visual_extractor.py
+++ modules/visual_extractor.py
@@ -21,7 +21,8 @@ class VisualExtractor(nn.Module):
             print(self.model)
             raise(NotImplementedError)
         elif args.original == False:
-            print(model)
+            print(self.model)
+            print(model.children())
             raise(NotImplementedError)
         else:
             raise(NotImplementedError)
