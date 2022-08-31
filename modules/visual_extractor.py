075aaefeb94c7c967afa52123248d7832a2e81c6
Kameradna <pjicasper@gmail.com>
2022-08-31 17:00:42 +1000

    editing the forward method of the visual extractor to extract local features as opposed to the global embedded classification token

--- modules/visual_extractor.py
+++ modules/visual_extractor.py
@@ -26,9 +26,10 @@ class VisualExtractor(nn.Module):
             raise(NotImplementedError)
 
     def forward(self, images):
-        patch_feats = self.model(images)
+        
 
         if self.original == True:
+            patch_feats = self.model(images)
             print(f'patch_feats.shape() = {patch_feats.shape}')# patch_feats.shape() = torch.Size([16, 2048, 7, 7])
             avg_feats = self.avg_fnt(patch_feats).squeeze().reshape(-1, patch_feats.size(1))#averages across the patch features
             print(f'avg_feats.shape() = {avg_feats.shape}')# avg_feats.shape() = torch.Size([16, 2048])
@@ -38,13 +39,37 @@ class VisualExtractor(nn.Module):
             print(f'patch_feats.shape() = {patch_feats.shape}')#patch_feats.shape() = torch.Size([16, 49, 2048])
 
         elif self.original == False:
-            print(f'patch_feats.shape() = {patch_feats.shape}')# patch_feats.shape() = torch.Size([16, 768])
-            avg_feats = patch_feats.squeeze().reshape(-1, patch_feats.size(1)) #likely does nothing to the shape, to be tested
-            print(f'avg_feats.shape() = {avg_feats.shape}')# avg_feats.shape() = torch.Size([16, 768])
-            batch_size, feat_size = patch_feats.shape
-            print(f'batch size = {batch_size}, feat_size = {feat_size}')#batch size = 16, feat_size = 768
-            patch_feats = patch_feats.reshape(batch_size, feat_size, -1).permute(0, 2, 1) #likely does nothing at all since patch feats is unused downstream, and the permute step may be internally done
-            print(f'patch_feats.shape() = {patch_feats.shape}')#patch_feats.shape() = torch.Size([16, 1, 768])
+            # patch_feats = self.model(images)
+            # print(f'patch_feats.shape() = {patch_feats.shape}')# patch_feats.shape() = torch.Size([16, 768])
+            # avg_feats = patch_feats.squeeze().reshape(-1, patch_feats.size(1)) #likely does nothing to the shape, to be tested
+            # print(f'avg_feats.shape() = {avg_feats.shape}')# avg_feats.shape() = torch.Size([16, 768]) confirmed does nothing
+            # batch_size, feat_size = patch_feats.shape
+            # print(f'batch size = {batch_size}, feat_size = {feat_size}')#batch size = 16, feat_size = 768 the feature size is much lower
+            # patch_feats = patch_feats.reshape(batch_size, feat_size, -1).permute(0, 2, 1) #likely does nothing at all since patch feats is unused downstream, and the permute step may be internally done
+            # print(f'patch_feats.shape() = {patch_feats.shape}')#patch_feats.shape() = torch.Size([16, 1, 768])
+
+            #edited from the forward process of ViT
+            x = images
+            x = self._process_input(x)
+            n = x.shape[0]
+
+            # Expand the class token to the full batch
+            batch_class_token = self.class_token.expand(n, -1, -1)
+            x = torch.cat([batch_class_token, x], dim=1)
+
+            x = self.encoder(x)
+
+            ####
+            all_feats = x
+            print(f'patch_feats.shape() = {all_feats.shape}')
+            ####
+
+            # Classifier "token" as used by standard language architectures
+            x = x[:, 0]
+
+            avg_feats = x
+
+            # x = self.heads(x) #nn.Identity anyway
 
 
 
