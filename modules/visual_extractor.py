031ae854744c11a2edfe52a01e88a17421642829
Kameradna <pjicasper@gmail.com>
2022-08-31 15:26:04 +1000

    working out what to prune from the transformer

--- modules/visual_extractor.py
+++ modules/visual_extractor.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -11,7 +12,7 @@ class VisualExtractor(nn.Module):
         self.pretrained = args.visual_extractor_pretrained
         model = getattr(models, self.visual_extractor)(pretrained=self.pretrained)#weights
         # model = models.get_model(self.visual_extractor, weights=self.weights)#the modern model registration feature, kameradna
-        print(args.original)
+        self.original = args.original
         if args.original == True:
             print("wowza")
             modules = list(model.children())[:-2]
@@ -31,11 +32,24 @@ class VisualExtractor(nn.Module):
 
     def forward(self, images):
         patch_feats = self.model(images)
-        print(f'patch_feats.shape() = {patch_feats.shape}')
-        avg_feats = self.avg_fnt(patch_feats).squeeze().reshape(-1, patch_feats.size(1))#averages across the patch features
-        print(f'avg_feats.shape() = {avg_feats.shape}')
-        batch_size, feat_size, _, _ = patch_feats.shape
-        print(f'batch size = {batch_size}, feat_size = {feat_size}')
-        patch_feats = patch_feats.reshape(batch_size, feat_size, -1).permute(0, 2, 1)
-        print(f'patch_feats.shape() = {patch_feats.shape}')
+
+        if self.original == True:
+            print(f'patch_feats.shape() = {patch_feats.shape}')
+            avg_feats = self.avg_fnt(patch_feats).squeeze().reshape(-1, patch_feats.size(1))#averages across the patch features
+            print(f'avg_feats.shape() = {avg_feats.shape}')
+            batch_size, feat_size, _, _ = patch_feats.shape
+            print(f'batch size = {batch_size}, feat_size = {feat_size}')
+            patch_feats = patch_feats.reshape(batch_size, feat_size, -1).permute(0, 2, 1)
+            print(f'patch_feats.shape() = {patch_feats.shape}')
+
+        elif self.original == False:
+            print(f'patch_feats.shape() = {patch_feats.shape}')
+            avg_feats = patch_feats.squeeze().reshape(-1, patch_feats.size(1)) #likely does nothing to the shape, to be tested
+            print(f'avg_feats.shape() = {avg_feats.shape}')
+            batch_size, feat_size, _, _ = patch_feats.shape
+            print(f'batch size = {batch_size}, feat_size = {feat_size}')
+            patch_feats = patch_feats.reshape(batch_size, feat_size, -1).permute(0, 2, 1) #likely does nothing at all since patch feats is unused downstream, and the permute step may be internally done
+            print(f'patch_feats.shape() = {patch_feats.shape}')
+            raise(NotImplementedError)
+
         return patch_feats, avg_feats
