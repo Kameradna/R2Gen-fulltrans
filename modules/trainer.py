c1dd49b51ac920180d5c444706477ccb8bf41137
Kameradna <pjicasper@gmail.com>
2022-09-01 21:13:18 +1000

    added better multi-gpu

--- modules/trainer.py
+++ modules/trainer.py
@@ -14,10 +14,10 @@ class BaseTrainer(object):
         # setup GPU device if available, move model into configured device
         self.device, device_ids = self._prepare_device(args.n_gpu)
         
-        if len(device_ids) > 1:
-            self.model = torch.nn.DataParallel(model, device_ids=device_ids)
-        # self.model = torch.nn.DataParallel(model) #by default just goes for it
-        self.model = model.to(self.device)#is this breaking multi-gpu?
+        # if len(device_ids) > 1:
+        #     self.model = torch.nn.DataParallel(model, device_ids=device_ids)
+        self.model = torch.nn.DataParallel(model) #by default just goes for it
+        # self.model = model.to(self.device)#is this breaking multi-gpu?
 
         self.criterion = criterion
         self.metric_ftns = metric_ftns
