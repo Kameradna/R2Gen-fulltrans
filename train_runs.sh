e6a472e4eb18fe7e976d9f0f520132eddbac44b0
Kameradna <pjicasper@gmail.com>
2022-09-06 23:15:16 +1000

    added run no 2 for all the ones that work (frozen,trans,pretrained) barring trans with training for pretrained and unpretrained examples

--- train_runs.sh
+++ train_runs.sh
@@ -31,6 +31,26 @@ python main_train.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
+--visual_extractor resnet101 \
+--d_vf 2048 \
+--monitor_metric CIDEr \
+--n_gpu 1 \
+--original \
+--record_dir recordsbaselinenopretrained1 \
+--save_dir resultsbaselinenopretrained1
+
+python main_train.py \
+--image_dir data/iu_xray/images/ \
+--ann_path data/iu_xray/annotation.json \
+--dataset_name iu_xray \
+--max_seq_length 60 \
+--threshold 3 \
+--batch_size 16 \
+--epochs 100 \
+--save_dir results/iu_xray \
+--step_size 50 \
+--gamma 0.1 \
+--seed 9223 \
 --visual_extractor vit_b_16 \
 --d_vf 768 \
 --monitor_metric CIDEr \
