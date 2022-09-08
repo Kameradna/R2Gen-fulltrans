054ba7e0d6454098d9aec6e5d2c5ca546e81a9a5
Kameradna <pjicasper@gmail.com>
2022-09-08 18:06:39 +1000

    frozen trans 3 first

--- train_runs.sh
+++ train_runs.sh
@@ -1,3 +1,24 @@
+python main_train.py \
+--image_dir data/iu_xray/images/ \
+--ann_path data/iu_xray/annotation.json \
+--dataset_name iu_xray \
+--max_seq_length 60 \
+--threshold 3 \
+--batch_size 16 \
+--epochs 100 \
+--save_dir results/iu_xray \
+--step_size 50 \
+--gamma 0.1 \
+--seed 9223 \
+--visual_extractor vit_b_16 \
+--weights IMAGENET1K_V1 \
+--d_vf 768 \
+--monitor_metric CIDEr \
+--n_gpu 1 \
+--frozen \
+--record_dir recordsfrozentrans3 \
+--save_dir resultsfrozentrans3
+
 
 python main_train.py \
 --image_dir data/iu_xray/images/ \
@@ -62,25 +83,5 @@ python main_train.py \
 --record_dir recordsfrozenbaseline3 \
 --save_dir resultsfrozenbaseline3
 
-python main_train.py \
---image_dir data/iu_xray/images/ \
---ann_path data/iu_xray/annotation.json \
---dataset_name iu_xray \
---max_seq_length 60 \
---threshold 3 \
---batch_size 16 \
---epochs 100 \
---save_dir results/iu_xray \
---step_size 50 \
---gamma 0.1 \
---seed 9223 \
---visual_extractor vit_b_16 \
---weights IMAGENET1K_V1 \
---d_vf 768 \
---monitor_metric CIDEr \
---n_gpu 1 \
---frozen \
---record_dir recordsfrozentrans3 \
---save_dir resultsfrozentrans3
 
 nvtop
