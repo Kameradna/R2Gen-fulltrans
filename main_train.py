553ca65893c4954587d0b58fade72639a2c0c908
Kameradna <pjicasper@gmail.com>
2022-09-10 23:28:21 +1000

    explicitly adding model to all gpus

--- main_train.py
+++ main_train.py
@@ -123,7 +123,6 @@ def main():
     #playground
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.nn.DataParallel(model)
-    model = model.to(device)
 
     print(model.parameters())
 
