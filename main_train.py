06705238fb21180a6746fa2c9a35696f44c3baaf
Kameradna <pjicasper@gmail.com>
2022-09-10 16:07:40 +1000

    debug outputs

--- main_train.py
+++ main_train.py
@@ -122,7 +122,7 @@ def main():
     optimizer = build_optimizer(args, model)
     lr_scheduler = build_lr_scheduler(args, optimizer)
 
-    # model = model.to(device,non_blocking=True)#after the dataparallel?
+    model = model.to(device)#after the dataparallel?
 
     # build trainer and start to train
     trainer = Trainer(model, criterion, metrics, optimizer, args, lr_scheduler, train_dataloader, val_dataloader, test_dataloader, device)
