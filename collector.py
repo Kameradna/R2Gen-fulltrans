5acea2dc2c1a336ce0733c8433701220ab6ab77e
Kameradna <pjicasper@gmail.com>
2022-09-23 15:30:33 +1000

    collector mo powerful

--- collector.py
+++ collector.py
@@ -9,7 +9,7 @@ for root, dir, file in os.walk(os.path.curdir):
     if len(file) > 0:
         if file[0] == "iu_xray.csv":
             file_info = pd.read_csv(f"{root}/{file[0]}")
-            run_name = pd.DataFrame([root.split("/")[-1][:-2]]*len(file_info.index))
+            run_name = pd.DataFrame([root.split("/")[-1][:-2]]*len(file_info.index),column=['name'])
             file_info = pd.concat([run_name,file_info],axis=1)
             mega_results = pd.concat([mega_results,file_info],axis=0)
 
@@ -17,6 +17,7 @@ print(mega_results)
 
 # #find the mean and std deviation for each name
 # mega_results[mega_results['name']]
+grouped_results = mega_results.groupby(['name']).mean()
 
 
 mega_results.to_csv("all_results.csv",index=False)
