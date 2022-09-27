cf90d4668e4f8ad671adcb46589f8d195e3b14dc
Kameradna <pjicasper@gmail.com>
2022-09-27 14:02:47 +1000

    trying to get all the collector info working

--- collector.py
+++ collector.py
@@ -32,7 +32,7 @@ for root, dir, file in os.walk(os.path.curdir):
             file_info = pd.read_csv(f"{root}/{file[0]}")
             name_eg = root.split("/")[-1]
             if name_eg in name_get:
-                name_eg = name_get(name_eg)
+                name_eg = name_get[name_eg]
             run_name = "".join([name_eg.split('_')[:-1]])
             run_name_series = pd.DataFrame((run_name)*len(file_info.index),columns=['name'])#padding to add to df
             file_info = pd.concat([run_name_series,file_info],axis=1)
