fb16b6e418ac64349058a0343671b5ea18809285
Kameradna <pjicasper@gmail.com>
2022-10-01 20:31:41 +1000

    Better structure

--- pretrain.py
+++ pretrain.py
@@ -1,24 +1,30 @@
-from sklearn.model_selection import ParameterGrid
-import multiprocessing
-from multiprocessing import Process
-import argparse
-import torchvision.models as models
-from copy import deepcopy
+# from sklearn.model_selection import ParameterGrid
+# import multiprocessing
+# from multiprocessing import Process
+
+import bit_pytorch.models as models
 
 #wget http://download.cs.stanford.edu/deep/CheXpert-v1.0-small.zip
-#into the data folder
 #unzip CheXpert-v1.0-small.zip -d data
 
+from bit_pytorch import train
+import bit_common
 
-def main():
-    print("Hello World!")
+print("Finished imports, did anything parse?")
+
+def main(args):
+    train.main(args)
 
 if __name__ == '__main__':
-    multiprocessing.set_start_method('spawn')
-    parser2 = argparse.ArgumentParser()
-    parser2.add_argument('--offset', type=int, default=0, help='run offset')
-    parser2.add_argument('--runs', type=int, default=1, help='runs')
-    args2 = parser2.parse_args()
-    offset = args2.offset
-    runs = args2.runs
-    main()
+    parser = bit_common.argparser(models.KNOWN_MODELS.keys())
+    parser.add_argument("--datadir", required=True,
+                        help="Path to the ImageNet data folder, preprocessed for torchvision.")
+    parser.add_argument("--workers", type=int, default=8,
+                        help="Number of background threads used to load data.")
+    parser.add_argument("--no-save", dest="save", action="store_false")
+    # parser.add_argument("--use_amp", dest="use_amp",action="store_true",
+    #                    help="Use Automated Mixed Precision to save potential memory and compute?")
+    parser.add_argument("--annodir", required=True, help="Where are the annotation files to load?")
+    parser.add_argument("--chexpert", dest="chexpert", action="store_true",help="Run as the chexpert paper?")
+    parser.add_argument("--pretrained", dest="pretrained", action="store_true",help="Do you want a pretrained network?")
+    main(parser.parse_args())
