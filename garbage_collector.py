1c0140531e2403150f48005644729bd9baf8eee7
Kameradna <pjicasper@gmail.com>
2022-10-08 23:48:45 +1100

    helpful garbage collection

--- garbage_collector.py
+++ garbage_collector.py
@@ -1,8 +1,15 @@
 import os
 import fnmatch
 
+
+
 if __name__ == '__main__':
+    this_dir = None
+
     for root, dir, files in os.walk('.'):
+        if thisdir != root:
+            ledger = []
+        thisdir = root
         for file in files:
-            if fnmatch.fnmatch(file,"*.pth.tar"):
-                    print(root, file)
+            if fnmatch.fnmatch(file,"*bit.pth.tar"):
+                    print(f"{root}/{file}")
