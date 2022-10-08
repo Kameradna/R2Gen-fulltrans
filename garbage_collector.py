738634317a773cbc820dfcc929c292234f87e9e9
Kameradna <pjicasper@gmail.com>
2022-10-08 23:51:36 +1100

    helpful garbage collection

--- garbage_collector.py
+++ garbage_collector.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
     this_dir = None
 
     for root, dir, files in os.walk('.'):
-        if root != this_dir:
-            print(f"Entering {root}")
-            ledger = []
-        this_dir = root
         for file in files:
+            if root != this_dir:
+                print(f"Entering {root}")
+                ledger = []
+            this_dir = root
             if fnmatch.fnmatch(file,"*bit.pth.tar"):
                     print("\\\\"+f"{root}/{file}")
