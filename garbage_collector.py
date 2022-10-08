dd0b693267ef8ba049abdfd87a396bfb9709cbb3
Kameradna <pjicasper@gmail.com>
2022-10-09 00:12:14 +1100

    helpful garbage collection

--- garbage_collector.py
+++ garbage_collector.py
@@ -5,12 +5,23 @@ import fnmatch
 
 if __name__ == '__main__':
     this_dir = None
-    ledger = "Not here yet"
+    ledger = {'auroc': [], 'f1': []}
+
     for root, dir, files in os.walk('.'):
         for file in files:
             if fnmatch.fnmatch(file,"*bit.pth.tar"):
                 if root != this_dir:
-                    print(ledger)
+                    if len(ledger['auroc']) > 0:
+                        #sort the ledger values and delete the lower 50% of cases
+                        ledger['auroc'].sort()
+                        centre_value = ledger['auroc'][len(ledger['auroc'])//2]
+                        for deletion_candidate in files:
+                            if float(deletion_candidate.split("_")[0]) < centre_value:
+                                print(f"would remove {deletion_candidate}")
+                                #os.remove(f"{this_dir}/{deletion_candidate}")
+
+
+
                     print(f"Entering {root}")
                     ledger = {'auroc': [], 'f1': []}
                 this_dir = root
