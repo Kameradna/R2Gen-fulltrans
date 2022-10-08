598baa56a069dddb72ed5c7a20cd65906ef2071d
Kameradna <pjicasper@gmail.com>
2022-10-08 23:39:47 +1100

    helpful garbage collection

--- garbage_collector.py
+++ garbage_collector.py
@@ -4,4 +4,4 @@ import fnmatch
 if __name__ == '__main__':
     for root, dir, files in os.walk('.'):
         if fnmatch.fnmatch(files,".pth"):
-            print(files)
+            print(file for file in files)
